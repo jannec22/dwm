9a89f66638909bf876526805a07d3bf2085a41c4
jannec22 <jannec1290@gmail.com>
2021-03-12 17:35:23 +0100

    gaps

--- config.h
+++ config.h
@@ -5,12 +5,12 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static const unsigned int borderpx = 8; /* border pixel of windows */
+static const unsigned int borderpx = 0; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 20;  /* horiz inner gap between windows */
-static const unsigned int gappiv = 10;  /* vert inner gap between windows */
-static const unsigned int gappoh = 10; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov = 10; /* vert outer gap between windows and screen edge */
+static const unsigned int gappih = 0;  /* horiz inner gap between windows */
+static const unsigned int gappiv = 0;  /* vert inner gap between windows */
+static const unsigned int gappoh = 0; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 0; /* vert outer gap between windows and screen edge */
 static const int swallowfloating = 0; /* 1 means swallow floating windows by default */
 static const int smartgaps = 0; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
